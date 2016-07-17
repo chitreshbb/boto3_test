ce65728948b72d25c750749c43d215dbdc5af297
Chris Smith <cleesmith2006@gmail.com>
2016-07-17 14:50:10 -0400

    test fetch_title with results in SQS queue

--- invoke.py
+++ invoke.py
@@ -3,15 +3,44 @@ import json
 import boto3
 
 client = boto3.client('lambda')
+
+json_params = {
+  # 'url': 'http://www.ghtctheatres.com/location/41139/Lewisburg-Cinema-8',
+  'url': 'http://cleesmith.github.io/health.html',
+  'queue': 'clsq'
+  # 'queue': ''
+}
+
+# is a param present?
+# q_param = json_params.get('queue')
+# if q_param is None or len(q_param) <= 0:
+#   print('not found!')
+# else:
+#   print('found')
+
+# (1) invoke a lambda function and expect a response:
 # resp = client.invoke(
 #   FunctionName='fetch_title',
 #   InvocationType='RequestResponse',
-#   Payload='{"url":"http://www.ghtctheatres.com/location/41139/Lewisburg-Cinema-8"}'
+#   Payload=json.dumps(json_params)
 # )
-resp = client.invoke(
-  FunctionName='lambda_invoker',
-  InvocationType='RequestResponse',
-  Payload='{"ignored":"nothing"}'
-)
+
+# (2) invoke a lambda function which then invokes another lambda function:
+# resp = client.invoke(
+#   FunctionName='lambda_invoker',
+#   InvocationType='RequestResponse',
+#   Payload='{"ignored":"nothing"}'
+# )
+
+# (3) invoke a lambda function asynchronously
+#     i.e. no response is returned,
+#     instead view the SQS queue to see the results:
+for x in range(0, 10):
+  resp = client.invoke(
+    FunctionName='fetch_title',
+    InvocationType='Event',
+    Payload=json.dumps(json_params)
+  )
+
 print(resp)
 print(resp['Payload'].read())
